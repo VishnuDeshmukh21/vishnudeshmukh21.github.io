32f3ddeddecb7331b41addb28d99bc695972bcc0
vishnu <vishnudeshmukh1222@gmail.com>
2025-03-17 02:04:24 +0530

    Revamped portfolio with animations

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("Portfolio Loaded");
+  const rainContainer = document.querySelector(".rain");
+
+  for (let i = 0; i < 50; i++) {
+      let rainDrop = document.createElement("span");
+      rainDrop.innerText = "0 1".charAt(Math.floor(Math.random() * 2));
+      rainDrop.className = "rain-drop";
+      rainDrop.style.left = Math.random() * 100 + "vw";
+      rainDrop.style.animationDuration = (Math.random() * 3 + 2) + "s";
+      rainContainer.appendChild(rainDrop);
+  }
 });
