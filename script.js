5ad1e22ec19aa8e43eff6f90548e6eed1ddff8d4
vishnu <vishnudeshmukh1222@gmail.com>
2025-03-17 02:08:00 +0530

    Updated minimalist portfolio

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const rainContainer = document.querySelector(".rain");
+// document.addEventListener("DOMContentLoaded", function() {
+//   const rainContainer = document.querySelector(".rain");
 
-  for (let i = 0; i < 50; i++) {
-      let rainDrop = document.createElement("span");
-      rainDrop.innerText = "0 1".charAt(Math.floor(Math.random() * 2));
-      rainDrop.className = "rain-drop";
-      rainDrop.style.left = Math.random() * 100 + "vw";
-      rainDrop.style.animationDuration = (Math.random() * 3 + 2) + "s";
-      rainContainer.appendChild(rainDrop);
-  }
-});
+//   for (let i = 0; i < 50; i++) {
+//       let rainDrop = document.createElement("span");
+//       rainDrop.innerText = "0 1".charAt(Math.floor(Math.random() * 2));
+//       rainDrop.className = "rain-drop";
+//       rainDrop.style.left = Math.random() * 100 + "vw";
+//       rainDrop.style.animationDuration = (Math.random() * 3 + 2) + "s";
+//       rainContainer.appendChild(rainDrop);
+//   }
+// });
